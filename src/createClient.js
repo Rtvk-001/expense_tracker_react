6cd3be3a2180dafcca4f4e7cd7ba337516148e76
Rithvik Raj <79125456+Rtvk-001@users.noreply.github.com>
2024-08-03 22:51:04 +0530

    Delete src/createClient.js

--- src/createClient.js
+++
@@ -1,6 +0,0 @@
-import {createClient} from "@supabase/supabase-js"
-export const supabase = createClient
-(
-    "https://iogaelmmphqmiahkfogh.supabase.co",
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvZ2FlbG1tcGhxbWlhaGtmb2doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjIxNTcxNTEsImV4cCI6MjAzNzczMzE1MX0.z12lr2UvoQoHPEI5oeq3f_6lXTawphfa57KEJ3vEC_4"
-);
